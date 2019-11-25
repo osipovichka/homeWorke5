0c3053011b257e92f7be16daa6aea533dd12e980
osipovichka <yan-vichka@mail.ru>
2019-11-25 16:57:32 +0300

    1 блок не доделана последняя задача, блок 2 не сделаны 2 последнии задачи

--- block1.js
+++ block1.js
@@ -1,6 +1,6 @@
 //6. Найти сумму четных чисел и их количество в диапазоне от 1 до 99
 
-function calcSumEndQuantityEvenNumbersBetween(start, end) {
+function calcSumAndQuantityEvenNumbersBetween(start, end) {
     let sumEvenNumbers = 0;
     let quantityEvenNumbers = 0;
 
@@ -17,24 +17,79 @@ function calcSumEndQuantityEvenNumbersBetween(start, end) {
     }
 
     return result;
-};
-//console.log(calcSumEndQuantityEvenNumbersBetween(1, 99));
+}
+//console.log(calcSumAndQuantityEvenNumbersBetween(1, 99));
 
 //7. Проверить простое ли число? (число называется простым, если оно делится только само на себя и на 1)
 
 function checkPrimeNumber(number) {
 
+    if (number < 1) {
+        return 'Число не натуральное, введено не правильное значение';
+    }
+
     if (number > 1) {
         for (i = 2; i < number; i++) {
             if (number % i === 0) {
-                return ('Число составное');
-            } else {
-                return ('Число простое');
+                return 'Число составное';
             }
         }
     }
-};
-//console.log(checkPrimeNumber(8));
+
+    return 'Число простое';
+}
+//console.log(checkPrimeNumber(25));
+
+//7.5 Найти корень натурального числа с точностью до целого (рассмотреть вариант последовательного подбора и метод бинарного поиска)
+
+function calcSqrtSequential(number) {
+    if (number < 0) {
+        return 'Число не натуральное, введено не правильное значение';
+    }
+
+    if (number === 1 || number === 0) {
+        return number;
+    }
+
+    for (i = 2; i < number; i++) {
+        let square = Math.pow(i, 2);
+        if (square === number) {
+            return i;
+        } else if (square > number) {
+            return --i;
+        }
+    }
+}
+//console.log(calcSqrtSequential(17));
+
+function calcSqrtBenary(number) {
+    if (number < 0) {
+        return 'Число не натуральное, введено не правильное значение';
+    }
+
+    if (number === 1 || number === 0) {
+        return number;
+    }
+
+    let start = 2;
+    let end = number;
+
+    while (true) {
+        let middle = start + Math.floor((end - start) / 2);
+        if (middle === start) {
+            return middle;
+        }
+        let squareMiddle = Math.pow(middle, 2);
+        if (squareMiddle === number) {
+            return middle;
+        } else if (squareMiddle > number) {
+            end = middle;
+        } else if (squareMiddle < number) {
+            start = middle;
+        }
+    }
+}
+//console.log(calcSqrtBenary(1854))
 
 //9. Посчитать сумму цифр заданного числа
 
@@ -47,7 +102,7 @@ function calcSumDigitsNumber(number) {
     }
 
     return sum;
-};
+}
 //console.log(calcSumDigitsNumber(198806));
 
 //8. Вычислить факториал числа n
@@ -55,39 +110,55 @@ function calcSumDigitsNumber(number) {
 function calcFactorialNumber(number) {
     let result = 1;
 
-    for (i = 1; i <= number; i++) {
+    for (i = 2; i <= number; i++) {
         result *= i;
     }
 
     return result;
-};
+}
 //console.log(calcFactorialNumber(5));
 
-//12. Посчитать количество нечетных элементов массива
+//10. Вывести число, которое является зеркальным отображением последовательности цифр заданного числа 1547963248596524898525477353636555.
 
-function randomArray(quantityElements) {
-    let randomNumber = 0;
-    let arr = [];
 
-    for (i = 0; i < quantityElements; i++) {
-        randomNumber = Math.floor(Math.random() * 50);
-        arr.push(randomNumber);
-    }
+// function getInvertedNumber(number) {
+//     let elements = 0; // Кол-во цифр
+//     let invertedNum = 0; //складывается перевернутое число
+//     let a = number;
+//     do {
+//         a /= 10;
+//         elements++;
+//     } while (a >= 1)
 
-    return arr;
+//     while (elements !== 0) {
+//         let numeral = number % 10;
+//         invertedNum += numeral * Math.pow(10, elements - 1);// присваевает разряд последней цифре от первой
+//         number = Math.floor(number / 10);
+//         elements -= 1;
+//     } return invertedNum;
+// }
+// console.log(getInvertedNumber(584269872354861548796248521379));
+
+
+function getInvertedNumber(number) {
+    let elements = 0; // Кол-во элементов
+    let invertedNum = 0; //складывается перевернутое число
+    let a = number;
+    let b = []; // пробую через массивы
+
+    do {
+        a /= 10;
+        elements++;
+    } while (a >= 1)// кол-во элементов в массиве
+
+    for (i = 0; i <= elements + 1; i++) {
+        let numeral = number % 10; 
+        b.push(numeral);
+        number = Math.floor(number / 10);
+        elements -= 1;
+    } return b;
+
+
+
 }
-let array = randomArray(15);
-
-function calcQuantityOddElementsАrray(arr) {
-    let quantityElements = 0;
-
-    for (i = 0; i <= arr.length; i++) {
-
-        if (i % 2 != 0) {
-            quantityElements++;
-        }
-    }
-
-    return quantityElements;
-};
-console.log(calcQuantityOddElementsАrray(array));
+console.log(getInvertedNumber(12584697325487623014));
---
+++ block2.js
@@ -0,0 +1,83 @@
+function randomArray(quantityElements) {
+    let randomNumber = 0;
+    let arr = [];
+
+    for (i = 0; i < quantityElements; i++) {
+        randomNumber = Math.floor(Math.random() * 50);
+        arr.push(randomNumber);
+    }
+
+    return arr;
+}
+let array = randomArray(10);
+console.log(array);
+
+//11. Сделать реверс массива (массив в обратном направлении)
+
+// function getReversedArray (arr) {
+//     let reverseArray = arr.reverse();
+//     return reverseArray;
+// }
+
+//console.log(getReversedArray(array));
+
+function getReversedArray(arr) {
+    let array = [];
+    for (i = arr.length - 1; i >= 0; i--) {
+        array.push(arr[i]);
+    }
+    return array;
+}
+//console.log(getReversedArray(array));
+
+//12. Посчитать количество нечетных элементов массива
+
+function calcQuantityOddElementsАrray(arr) {
+    let quantityElements = 0;
+
+    for (i = 0; i <= arr.length; i++) {
+
+        if (i % 2 != 0) {
+            quantityElements++;
+        }
+    }
+
+    return quantityElements;
+}
+//console.log(calcQuantityOddElementsАrray(array));
+
+//13. Поменять местами первую и вторую половину массива.
+
+function calcArray(arr) {
+
+    for (i = 0; i < arr.length / 2; i++) {
+        let a = arr[i];
+        arr[i] = arr[arr.length - i - 1];
+        arr[arr.length - i - 1] = a;
+    }
+    return arr;
+}
+//console.log(calcArray(array));
+
+//14. Отсортировать массив пузырьком (Bubble)
+
+function calcBubbleSort(arr) {
+    for (i = 0; i < arr.length - 1; i--) {
+        let wasSorting = false;
+
+        for (j = 0; j < arr.length - 1 - i; j++) {
+
+            if (arr[j] > arr[j+1]) {
+                let a = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = a;
+                wasSorting = true;
+            }
+        }
+        if (!wasSorting) break;
+    }
+    return arr;
+}
+//console.log(calcBubbleSort(array));
+
+//15.Отсортировать массив ещё двумя методами (выбором (Select), вставками (Insert)) 
